19fd6e3aa3707207f1c1a8434e0b319a2251b699
ColePresnell <cole.presnell@gmail.com>
2018-05-22 09:47:32 -0500

    finished inclase activity

---
+++ twoServers.js
@@ -0,0 +1,26 @@
+var http = require("http");
+
+var PORTONE = 7000;
+
+var PORTTWO = 7500;
+
+function handleRequestOne(request, response) {
+    response.end("You are a fun person to be around!" + request.url);
+}
+
+function handleRequestTwo(request, response) {
+    response.end("You have a lot of commitment issues and are passive aggressive" + request.url);
+}
+
+var serverOne = http.createServer(handleRequestOne);
+
+var serverTwo = http.createServer(handleRequestTwo);
+
+serverOne.listen(PORTONE, function() {
+    console.log("Server listening on http://localhost:" + PORTONE);
+});
+
+serverTwo.listen(PORTTWO, function() {
+    console.log("Server listening on http://localhost:" + PORTTWO);
+});
+
